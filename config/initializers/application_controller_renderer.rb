c0896971f0e0d916632199a6592009a38494938e
伊藤　太志 <ito_taishi@itoutis.local>
2018-10-11 14:56:32 +0900

    [start&finish]4-1~7

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '0364b2d2f0ff21a9a801c31b3eac01d71349e85451bf4189a9e3b361ddc24afe007c5966b74b18b46447832236b9047eb076230fa47bc374510c90d74cf74987'
